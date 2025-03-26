3cd97d8af3c16d79dd36b00d032ba91d226f1a2e
noorhatem123 <noor.kadry22d@eslsca.edu.eg>
2025-03-26 09:53:02 +0200

    added some details

---
+++ VR_art_gallery/Assets/Scripts/interaction/interactor.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private Transform _interactionPoint;
+    [SerializeField] private float _interactionPointRadius =0.5f;
+    [SerializeField] private LayerMask _interactableMask;
+
+    private readonly Collider[] _colliders = new Collider[3];
+    [SerializeField] private int _numFound;
+
+    private void Update()
+    {
+        _numFound = Physics.OverlapSphereNonAlloc(_interactionPoint.position, _interactionPointRadius, _colliders, _interactableMask);
+    }
+}
